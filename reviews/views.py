b44bb829582d71ace2ea34d59954355cd964cce1
Caleb Miller <caleb@mathmammoth.com>
2021-10-15 13:25:26 -0600

    last commit

--- reviews/views.py
+++ reviews/views.py
@@ -1,26 +1,44 @@
-from django.http.response import HttpResponse
+from typing import Any, Dict
+from django.http.request import HttpRequest
+from django.http.response import HttpResponse, HttpResponseRedirect
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView
-from django.views.generic.base import TemplateView
+from django.views.generic.base import TemplateView, View
 
 from .models import Review
 from .forms import ReviewForm
+
 
 class ReviewView(CreateView):
     form_class = ReviewForm
     template_name = "reviews/review.html"
     success_url = "/thank-you"
 
+
 class ThankYouView(TemplateView):
     template_name = "reviews/thank_you.html"
+
 
 class ReviewsListView(ListView):
     template_name = "reviews/review_list.html"
     model = Review
     context_object_name = 'reviews'
 
+
 class SingleReviewView(DetailView):
     template_name = "reviews/single_review.html"
     model = Review
     context_object_name = 'review'
+
+    def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        context["is_favorite"] = self.request.session.get("favorite_review") == str(self.object.id)
+        return context
+
+
+class AddFavoriteView(View):
+    def post(self, request: HttpRequest) -> HttpResponse:
+        review_id = request.POST["review_id"]
+        request.session["favorite_review"] = review_id
+        return HttpResponseRedirect("/reviews/" + review_id)
